ab468d43abe3bf6e678b1ba3328d729d3a4c0d39
Isaranwa <pennyisaranwa@gmail.com>
2023-10-28 19:34:21 +0300

    Added animation to span

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        <div className="name">
-          <span>Peninah Isaranwa</span>
-        </div>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Peninah Isaranwa
+        </motion.span>
         <div className="social">
           <a href="#">
             <img src="/facebook.png" alt="/"></img>

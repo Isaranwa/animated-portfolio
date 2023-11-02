41dca78f9fe6cd7525e5c33d7d8d7d45b023da87
Isaranwa <pennyisaranwa@gmail.com>
2023-11-02 16:44:03 +0300

    Add width of bg

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -28,7 +28,13 @@ const Links = () => {
   return (
     <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <motion.a href={`#${item}`} key={item} variants={itemVariants}>
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.3 }}
+          whileTap={{ scale: 0.8 }}
+        >
           {item}
         </motion.a>
       ))}
